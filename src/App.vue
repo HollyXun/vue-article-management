66fc334db4b11498558a8b0649e224bbf2a109c0
Beats <15018263165@163.com>
2021-06-18 22:31:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+})
+</script>
+
+<style scoped>
+</style>
+
+
+
+
