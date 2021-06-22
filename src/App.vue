1bd4651de404ed0db8073c099d56af6b12fa4d6f
Beats <15018263165@163.com>
2021-06-22 22:29:24 +0800

    add ColumnList.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="container" id="app">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+
+const testData:ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1专栏',
+    description: '描述文本',
+    avatar: 'http://sc.adminbuy.cn/uploads/allimg/210326/1-2103261344300-L.png'
+  },
+  {
+    id: 2,
+    title: 'test2',
+    description: '描述文本',
+    avatar: 'http://sc.adminbuy.cn/uploads/allimg/210326/1-210326134G20-L.png'
+  }
+]
 
 export default defineComponent({
   name: 'App',
   components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
   }
 })
 </script>
