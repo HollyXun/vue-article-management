b55959b5d99bac7c76b266a5274b46185c2ed250
Beats <15018263165@163.com>
2021-06-27 00:05:54 +0800

    初步添加dropdown组件

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="dropdown">
+    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="isToggleOpen">
+      {{ title }}
+    </a>
+    <ul class="dropdown-menu" :style="dropdownMenu" v-if="isOpen">
+      <li><a class="dropdown-item" href="#">Action</a></li>
+      <li><a class="dropdown-item" href="#">Another action</a></li>
+      <li><a class="dropdown-item" href="#">Something else here</a></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Dropdown',
+  props: {
+    title: {
+      type: String,
+      require: true
+    }
+  },
+  setup () {
+    const dropdownMenu = reactive({
+      display: 'block'
+    })
+    const isOpen = ref(false)
+    const isToggleOpen = () => {
+      isOpen.value = !isOpen.value
+    }
+    return {
+      isOpen,
+      isToggleOpen,
+      dropdownMenu
+    }
+  }
+
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -7,7 +7,9 @@
         <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
       </ul>
       <ul v-else class="list-inline mb-0">
-        <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">你好，{{ user.name }}</a></li>
+        <li class="list-inline-item">
+          <dropdown :title="`你好，${user.name}`"></dropdown>
+        </li>
       </ul>
     </div>
 
@@ -16,6 +18,7 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import Dropdown from './Dropdown.vue'
 
 export interface UserProps {
   isLogin: boolean;
@@ -25,6 +28,9 @@ export interface UserProps {
 
 export default defineComponent({
   name: 'GlobalHeader',
+  components: {
+    Dropdown
+  },
   props: {
     user: {
       type: Object as PropType<UserProps>,
