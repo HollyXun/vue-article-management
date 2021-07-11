5ef3a862edb30973100f058d0b6557b394fc3f8c
Beats <15018263165@163.com>
2021-07-11 17:45:11 +0800

    Fix Bug

--- src/components/GlobalFooter.vue
+++ src/components/GlobalFooter.vue
@@ -2,7 +2,7 @@
 <footer class="text-center py-4 text-secondary bg-light mt-5">
   <small>
     <ul class="list-inline mb-0">
-      <ul class="list-inline-item">© 2021 Best</ul>
+      <ul class="list-inline-item">Copyright © 2021 Best. All rights reserved. </ul>
       <ul class="list-inline-item">文档</ul>
       <ul class="list-inline-item">联系</ul>
       <ul class="list-inline-item">更多</ul>
